fade0cda9e88a0d224abe51e6e0fd09993bc3faa
ozaki <koki.ozaki98@gmail.com>
2023-09-26 04:22:40 +0900

    一旦作成

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>ECサイト</title>
+  <link rel="stylesheet" href="src/style/style.css">
+</head>
+<body>
+  
+</body>
+</html>
