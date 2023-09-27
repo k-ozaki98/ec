7b9855e8be931b26f74164b8c596445d43467bc1
ozaki98 <k.ozaki@insel-art.jp>
2023-09-27 13:30:28 +0900

    header調整

--- index.php
+++ index.php
@@ -29,18 +29,22 @@
     <div class="header-nav__logo"><img src="src/images/common/logo.png" alt=""></div>
     <p class="header-nav__text"><span>こんにちは</span>お届け先を選択</p>
     <div class="header-nav__search">
-      <select name="" id="search">
-        <option value="カテゴリー01">カテゴリー01</option>
-        <option value="カテゴリー02">カテゴリー02</option>
-        <option value="カテゴリー03">カテゴリー03</option>
-        <option value="カテゴリー04">カテゴリー04</option>
-        <option value="カテゴリー05">カテゴリー05</option>
-        <option value="カテゴリー06">カテゴリー06</option>
-        <option value="カテゴリー07">カテゴリー07</option>
-        <option value="カテゴリー08">カテゴリー08</option>
-        <option value="カテゴリー09">カテゴリー09</option>
-      </select>
+      <div class="select-box">
+        <select name="" id="search">   
+          <option value="すべて">すべて</option>
+          <option value="カテゴリー01">カテゴリー01</option>
+          <option value="カテゴリー02">カテゴリー02</option>
+          <option value="カテゴリー03">カテゴリー03</option>
+          <option value="カテゴリー04">カテゴリー04</option>
+          <option value="カテゴリー05">カテゴリー05</option>
+          <option value="カテゴリー06">カテゴリー06</option>
+          <option value="カテゴリー07">カテゴリー07</option>
+          <option value="カテゴリー08">カテゴリー08</option>
+          <option value="カテゴリー09">カテゴリー09</option>
+        </select>
+      </div>
       <input type="text" placeholder="検索 Amazon.co.jp">
+      <input type="submit" value="検索">
     </div>
     <div class="header-nav__lang">JP</div>
     <p class="header-nav__login">
